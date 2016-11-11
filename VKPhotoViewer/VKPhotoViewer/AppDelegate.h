8a25b835064d08770b52a5a9a753d089c83dcf92
Piriev Timur <pirievtimur1@gmail.com>
2016-11-11 13:58:35 +0200

    Init commit

---
+++ VKPhotoViewer/VKPhotoViewer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  VKPhotoViewer
+//
+//  Created by Timur Piriev on 11/11/16.
+//  Copyright © 2016 Timur Piriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ VKPhotoViewer/VKPhotoViewer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VKPhotoViewer
+//
+//  Created by Timur Piriev on 11/11/16.
+//  Copyright © 2016 Timur Piriev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
